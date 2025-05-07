b998aef8ea261d5701028dbbf2e7b612260327c3
Ivan Hancco <isivanh@gmail.com>
2025-05-07 06:12:26 -0500

    chore(backend): remove unused classes for pokemon type

--- backend/src/types/pokemon-type.ts
+++ backend/src/types/pokemon-type.ts
@@ -1,5 +1,3 @@
-import { SimplePokemon } from "./pokemon";
-
 export interface PokemonTypeResponse {
   results: PokemonType[];
 }
@@ -7,18 +5,4 @@ export interface PokemonTypeResponse {
 export interface PokemonType {
   id: string;
   name: string;
-}
-
-export interface PokemonTypeResult {
-  id: number;
-  name: string;
-  pokemon?: {
-    pokemon: SimplePokemon;
-  }[];
-}
-
-export interface Type {
-  id: number;
-  name: string;
-  pokemons: SimplePokemon[];
 }
